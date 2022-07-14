131709e89057cf93bac67160fa8f38f716e62c27
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-07-14 11:22:25 +0200

    Update dev_run.py

--- dev_run.py
+++ dev_run.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('/Users/leonard/Library/Application Support/QGIS/QGIS3/profiles/default/python/plugins/osm_2_imm')
-
 from core.runner import main
 
 main(None, test = True)
